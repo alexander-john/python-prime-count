c59e9723e738ac9d8aafd8009552514626dfdea9
alexander-john <96aljohn@gmail.com>
2024-10-03 10:36:42 -0700

    Initial commit

---
+++ python-prime-count.py
@@ -0,0 +1,18 @@
+# primes.py
+
+def count_primes(n):
+    count = 0
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            count += 1
+    return count
+
+if __name__ == "__main__":
+    N = 10000
+    total_primes = count_primes(N)
+    print(f"Number of primes up to {N}: {total_primes}")
